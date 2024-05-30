e18a63b1949caf0dc36962a5e94360980ef3b69e
TinaKYF <>
2024-05-30 19:25:22 +1000

    modify mavigation

--- assets/js/plugins/jquery.greedy-navigation.js
+++ assets/js/plugins/jquery.greedy-navigation.js
@@ -13,15 +13,6 @@ var $hlinks = $('#site-nav .hidden-links');
 var breaks = [];
 
 function updateNav() {
-
-  var availableSpace = $btn.hasClass('hidden') ? $nav.width() : $nav.width() - $btn.width() - 30;
-
-  // Record the width of the list
-  breaks.push($vlinks.width());
-
-  // Move item to the hidden list
-  $vlinks.children('*:not(.masthead__menu-item--lg)').last().prependTo($hlinks);
-
   // Show the dropdown btn
 
   $btn.removeClass('hidden');
